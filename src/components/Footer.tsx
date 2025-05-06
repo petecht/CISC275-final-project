0a400d3d3f350a4e393508598aafdea898028a7d
pgedelman <pgedelman@gmail.com>
2025-05-06 16:25:09 -0400

    Added button to allow apikey changes and centered names

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-import '../styles/Navbar.css';
+import '../styles/Footer.css';
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
 
 let keyData = "";
 function Footer({saveKeyData, prevKey} : {saveKeyData: string, prevKey: string | null}) {
+    const [enterAPIKey, setEnterAPIKey] = useState<boolean>(prevKey === null);
     if (prevKey !== null) {
         keyData = JSON.parse(prevKey);
     }
@@ -17,24 +18,30 @@ function Footer({saveKeyData, prevKey} : {saveKeyData: string, prevKey: string |
         localStorage.setItem(saveKeyData, JSON.stringify(key));
     // window.location.reload(); // Removed reload as we'll handle state within React
     }
+    function handleEnterAPIKey() {
+        setEnterAPIKey(!enterAPIKey);
+    }
 
     //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
     function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
         setKey(event.target.value);
     }
   return (
-    <div>
-        <Form className="api-key-form">
+    <div className='footer'>
+        { enterAPIKey && <Form className="api-key-form">
             <Form.Label>API Key:</Form.Label>
-            <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+            <Form.Control className='control' type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
             <br></br>
             <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-        </Form>
+        </Form> }
       <div className="team-info">
-            <p>Ryan Koller</p>
-            <p>Peter Chapman</p>
-            <p>Shaurya Kumar</p>
-            <p>Paul Edelman</p>
+            <Button className='enter-api-key-button' onClick={handleEnterAPIKey}>ChangeAPIKey</Button>
+            <div className='names'>
+              <p>Ryan Koller</p>
+              <p>Peter Chapman</p>
+              <p>Shaurya Kumar</p>
+              <p>Paul Edelman</p>
+            </div>
       </div>
     </div>
   );
